5be103fbe7c05f60911aa7d0c053fb2871c0d9af
David Caçote Rodrigues <daccr_malluco@msn.com>
2020-04-09 15:18:26 +0100

    Update jogodasorte.py

--- jogodasorte.py
+++ jogodasorte.py
@@ -5,13 +5,13 @@ print("##################")
 print ("###  WELCOME  ###")
 print ("  GUESS GAME  ")
 print ("##################")
-print("Guess a number 0 to 100 That im thiking. Lets see if you have lucky.")
+print("Guess a number 0 to 100 That i'm thiking. Lets see if you have lucky.")
 
 numero_sorteado = random.randint(0,100)
 tentativas = 5
-nome = input("Antes diz-nos como te chamas: ")
-numero_jogador = int(input("Que número estou a pensar? "))
-print("Vamos comparar a tua resposta com o número sorteado. A pensar...")
+nome = input("Before, whats is your name: ")
+numero_jogador = int(input("What number i'm thinking? "))
+print("Let's compare your answer with my number. Looking...")
 time.sleep(2)
 
 while tentativas >= 1:
@@ -19,23 +19,23 @@ while tentativas >= 1:
     if numero_jogador <= 100:
         if numero_jogador == numero_sorteado:
             print("""####  JACKPOT  ####
-ACERTASTE EM CHEIO""")
+CONGRATS""")
             time.sleep(1)
             break
         elif numero_jogador < numero_sorteado:
-            print(nome + " referiste um número abaixo do sorteado")
-            print("Tens mais " + str(tentativas) + " tentativas")
-            numero_jogador = int(input("Que número estou a pensar? "))
+            print(nome + " you mention a number below mine")
+            print("You have more " + str(tentativas) + " tries")
+            numero_jogador = int(input("What number i'm thinking? "))
         elif numero_jogador > numero_sorteado:
-            print(nome + " referiste um número acima do sorteado")
-            print("Tens mais " + str(tentativas) + " tentativas")
-            numero_jogador = int(input("Que número estou a pensar? "))
+            print(nome + " you mention a number above mine")
+            print("You have more " + str(tentativas) + " tries")
+            numero_jogador = int(input("What number i'm thinking? "))
 
 
     else:
-        print("Tens mais " + str(tentativas) + " tentativas")
-        print(nome + " deste um número incorrecto. Tens de escrever um número entre 0 e 100")
-        numero_jogador = int(input("Que número estou a pensar? "))
+        print("You have more " + str(tentativas) + " tries")
+        print(nome + " you gave wrong number. The number must be between 0 and 100")
+        numero_jogador = int(input("What number i'm thinking? "))
         time.sleep(2)
 
 print("##################")
